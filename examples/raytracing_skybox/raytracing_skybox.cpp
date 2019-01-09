650bf3f22bd223190876685359ce2b8c7d186d9f
Xu Xing <xing.xu@intel.com>
2019-01-09 09:12:43 +0800

    Fix spirv gen error

--- examples/raytracing_skybox/raytracing_skybox.cpp
+++ examples/raytracing_skybox/raytracing_skybox.cpp
@@ -588,10 +588,10 @@ class VulkanExample : public VulkanExampleBase {
     std::array<VkPipelineShaderStageCreateInfo, 2> shaderStages;
 
     shaderStages[0] =
-        loadShader(getAssetPath() + "shaders/raytracing/texture.vert.spv",
+        loadShader(getAssetPath() + "shaders/raytracing_skybox/texture.vert.spv",
                    VK_SHADER_STAGE_VERTEX_BIT);
     shaderStages[1] =
-        loadShader(getAssetPath() + "shaders/raytracing/texture.frag.spv",
+        loadShader(getAssetPath() + "shaders/raytracing_skybox/texture.frag.spv",
                    VK_SHADER_STAGE_FRAGMENT_BIT);
 
     VkGraphicsPipelineCreateInfo pipelineCreateInfo =
